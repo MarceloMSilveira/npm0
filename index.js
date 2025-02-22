17d7acf13e82aefc928c36563e0f400bd3f25e86
Marcelo Silveira <marcelomendes.siveira@gmail.com>
2025-02-22 08:15:01 -0300

    set heroes

--- index.js
+++ index.js
@@ -1,6 +1,17 @@
 import generateName from 'sillyname'; 
+import superheroes from 'superheroes';
+
 
 
 const sillyName = generateName();
+function selectAHero(allSpHeroes) {
+    const qttHeroes = allSpHeroes.length;
+    console.log(`There are ${qttHeroes} heroes.`);
+    const choosedIndice = Math.floor(qttHeroes*Math.random());    
+    return allSpHeroes[choosedIndice];
+}
+
 
 console.log(`Meu nome é: ${sillyName}`);
+console.log(`Eu sou: ${selectAHero(superheroes)}`);
+
